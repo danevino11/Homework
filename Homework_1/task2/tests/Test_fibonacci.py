788a03eefd10cd3ad64086eb8380d6ce774bff94
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-17 23:43:26 +0300

    Update and rename Test_fibo.py to Test_fibonacci.py

---
+++ Homework_1/task2/tests/Test_fibonacci.py
@@ -0,0 +1,16 @@
+from typing import Sequence
+
+import pytest
+
+from Homework_1.task2 import Fibonacci
+
+
+@pytest.mark.parametrize(
+    ["test_input", "expected"],
+    [((1, 1, 2, 3, 5), True), ((-1245, -1000, -531, -34), False), 
+     ([0, 0, 0, 1],  False),  ((12, 13, 54, 83), False)]  
+)
+def test_fibonacci(test_input: Sequence, expected: bool):
+    actual = check_fibonacci(test_input)
+
+    assert actual == expected
