40b13770581edc75a38a0ce6d5aa19db3f57a83b
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-17 20:46:33 +0300

    Update test_calculator.py

--- Homework_1/task1/sample_project/test/test_calculator.py
+++ Homework_1/task1/sample_project/test/test_calculator.py
@@ -4,10 +4,10 @@ from Homework_1.task1.sample_project.calculator import calc
 
 
 @pytest.mark.parametrize(
-    ["number", "expected"],
-    [(65536, True), (12, False), (321, False), (-1245, False),]  # Two xtra numbers to check
+    ["test_input", "expected"],
+    [(65536, True), (12, False), (321, False), (-1245, False)]  # Two xtra numbers to check
 )
 def test_power_of_2(number: int, expected: bool):
-    actual = calc.check_power_of_2(number)
+    actual = calc.check_power_of_2(test_input)
 
     assert actual == expected
