3557b56e4b0260936b71b647e5ae80049c42e65f
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-17 23:26:16 +0300

    Create Test_fibo.py

---
+++ Homework_1/task2/tests/Test_fibo.py
@@ -0,0 +1,13 @@
+import pytest
+
+from Homework_1.task2 import Fibonacci
+
+
+@pytest.mark.parametrize(
+    ["test_input", "expected"],
+    [(65536, True), (12, False), (321, False), (-1245, False)]  # Two xtra numbers to check
+)
+def test_power_of_2(test_input: int, expected: bool):
+    actual = calc.check_power_of_2(test_input)
+
+    assert actual == expected
