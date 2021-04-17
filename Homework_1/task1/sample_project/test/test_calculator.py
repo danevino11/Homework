16a36ece84fa1e151b3570d41b1468942554c5ea
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-17 20:47:54 +0300

    Update test_calculator.py

--- Homework_1/task1/sample_project/test/test_calculator.py
+++ Homework_1/task1/sample_project/test/test_calculator.py
@@ -7,7 +7,7 @@ from Homework_1.task1.sample_project.calculator import calc
     ["test_input", "expected"],
     [(65536, True), (12, False), (321, False), (-1245, False)]  # Two xtra numbers to check
 )
-def test_power_of_2(number: int, expected: bool):
+def test_power_of_2(test_input: int, expected: bool):
     actual = calc.check_power_of_2(test_input)
 
     assert actual == expected
