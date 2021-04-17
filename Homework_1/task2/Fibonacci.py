780eb749dd514a436bacfaa430c3d38e778aac37
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-17 23:22:47 +0300

    Update Fibonacci.py

--- Homework_1/task2/Fibonacci.py
+++ Homework_1/task2/Fibonacci.py
@@ -10,4 +10,8 @@ from typing import Sequence
 
 
 def check_fibonacci(data: Sequence[int]) -> bool:
-    ...
+    for i in range(len(data) - 2):
+        if not data[i] + data[i + 1] == data[i + 2]:  # to exclude all the other cases
+            return False
+    return True
+    
