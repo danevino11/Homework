28ae3d0ae2879932c879eef0949078e946c24856
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-17 19:54:01 +0300

    Update test_calculator.py

--- Homework_1/task1/sample_project/test/test_calculator.py
+++ Homework_1/task1/sample_project/test/test_calculator.py
@@ -2,10 +2,12 @@ import pytest
 
 from Homework_1.task1.sample_project.calculator import calc
 
-def test_positive_case():
-    """Testing that actual powers of 2 give True"""
-    assert check_power_of_2(65536)
-    
-def test_negative_case():
-    """Testing that non-powers of 2 give False"""
-    assert not check_power_of_2(12)    
+
+@pytest.mark.parametrize(
+    ["number", "expected"],
+    [(65536, True), (12, False), (321, False), (-1245, False],  # Two xtra checks
+)
+def test_power_of_2(number: int, expected: bool):
+    actual_result = calc.check_power_of_2(number)
+
+    assert actual == expected
