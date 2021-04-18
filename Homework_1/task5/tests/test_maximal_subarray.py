04c46661036ead35a038bcf367e50a2fbf2270be
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-19 00:13:13 +0300

    Update and rename __init__.py to test_maximal_subarray.py

---
+++ Homework_1/task5/tests/test_maximal_subarray.py
@@ -0,0 +1,18 @@
+from typing import List
+
+import pytest
+
+from Homework_1.task5 import maximal_subarray_sum
+
+
+@pytest.mark.parametrize(
+    ["test_nums", "k", "expected"],
+    
+    [
+    ([-5, -4, 3, 4], 3, 7), 
+    ([-4, -5, -2, -3, 2], 4, 0) 
+    ]  
+)
+def test_maximal_subarray(test_nums, k, expected):
+    actual = maximal_subarray_sum.find_maximal_subarray_sum(test_nums, k)
+    assert actual == expected
