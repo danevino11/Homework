30c99a5e664ec9a4e7f286b0783cdcd03476fc91
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-18 18:36:16 +0300

    Update maximal_subarray_sum.py

--- Homework_1/task5/maximal_subarray_sum.py
+++ Homework_1/task5/maximal_subarray_sum.py
@@ -13,4 +13,12 @@ from typing import List
 
 
 def find_maximal_subarray_sum(nums: List[int], k: int) -> int:
-    ...
+    nums.sort()
+    last = nums[len(nums)-(k-2)]
+    second_last = nums[len(nums) - (k-1)]
+    third_last = nums[len(nums) - k]
+    if last >= last + second_last:    # in case the third-to-last element is less than/equal to zero
+        return last
+    if last + second_last >= last + second_last + third_last:    # in case the third-to-last and second-to-last elements are less than/equal to zero
+        return last + second_last
+    return  last + second_last + third_last
