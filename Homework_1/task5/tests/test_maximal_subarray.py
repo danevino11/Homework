91dd3d0add4994742c3001d36fa5d827c87ccfb2
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-20 19:42:57 +0300

    Update test_maximal_subarray.py

--- Homework_1/task5/tests/test_maximal_subarray.py
+++ Homework_1/task5/tests/test_maximal_subarray.py
@@ -10,7 +10,7 @@ from Homework_1.task5 import maximal_subarray_sum
     
     [
     ([-5, -4, 3, 4], 3, 7), 
-    ([-4, -5, -2, -3, 2], 4, 2),
+    ([-4, -5, -2, -3, 2], 4, 2),     # tests different cases
     ([1, -2, -6, -3, 1], 2, 2) 
     ]  
 )
