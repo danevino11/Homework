6b65a776509ec260be37a78e907ab94a143bab0c
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-25 23:58:24 +0300

    Update test_all_possible_lists.py

--- Homework_2/task3/tests/test_all_possible_lists.py
+++ Homework_2/task3/tests/test_all_possible_lists.py
@@ -7,8 +7,8 @@ from Homework_2.task3 import all_possible_lists
  
 @pytest.mark.parametrize(
     ["args", "expected"], 
-    [([2, 4], [4, 1], [9, 3], 
-    [2, 4, 9], 
+    [([2, 4], [4, 1], [9, 3]), 
+    ([2, 4, 9], 
     [2, 4, 3], 
     [2, 1, 9], 
     [2, 1, 3], 
