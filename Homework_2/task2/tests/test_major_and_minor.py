4c7212e0cbd24adde918e00383f51a9d294f846b
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-25 20:53:38 +0300

    Update test_major_and_minor.py

--- Homework_2/task2/tests/test_major_and_minor.py
+++ Homework_2/task2/tests/test_major_and_minor.py
@@ -7,7 +7,7 @@ from Homework_2.task2 import major_and_minor
  
 @pytest.mark.parametrize(
     ["values", "expected"], 
-    [[4, 5, 3, 5, 5, 5, 9, 5, 5, 5, 1, 2], [5, 4]]
+    [([4, 5, 3, 5, 5, 5, 9, 5, 5, 5, 1, 2], (5, 4)),]
 )
 def test_major_and_minor_elem(values: List[int], expected: Tuple[int, int]):
     assert major_and_minor.major_and_minor_elem(values) == expected
