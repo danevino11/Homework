3309c6e6c3f9519be4adcca93078d7cf2e955618
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-25 22:07:47 +0300

    Create test_range_function.py

---
+++ Homework_2/task5/test/test_range_function.py
@@ -0,0 +1,20 @@
+import string
+from typing import Any, List
+
+import pytest
+from Homework_2.task5 import range_function
+
+
+@pytest.mark.parametrize(
+    ["args", "expected"],
+    [
+        ([string.ascii_lowercase, "f"], ["a", "b", "c", "d", "e"]),
+        (
+            [string.ascii_lowercase, "f", "o"],
+            ["f", "g", "h", "i", "j", "k", "l", "m", "n"],
+        ),
+        ([string.ascii_lowercase, "p", "g", -2], ["p", "n", "l", "j", "h"]),
+    ],
+)
+def test_custom_range(*args: List, expected: List[Any]):
+    assert range_function.custom_range(*args) == expected
