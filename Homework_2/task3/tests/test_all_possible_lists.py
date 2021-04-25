4e11e2f8b2d53ccc4c1158484e6acbb03ea00197
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-26 00:17:36 +0300

    Update test_all_possible_lists.py

--- Homework_2/task3/tests/test_all_possible_lists.py
+++ Homework_2/task3/tests/test_all_possible_lists.py
@@ -8,18 +8,20 @@ from Homework_2.task3 import all_possible_lists
 @pytest.mark.parametrize(
     ["args", "expected"], 
     [
-     ([[2, 4], [4, 1], [9, 3]]), 
-    ([[2, 4, 9], 
-    [2, 4, 3], 
-    [2, 1, 9], 
-    [2, 1, 3], 
-    [4, 4, 9], 
-    [4, 4, 3], 
-    [4, 1, 9], 
-    [4, 1, 3]]),
-    
-    
-    ]
+        (
+            [[2, 4], [4, 1], [9, 3]],
+            [
+                [2, 4, 9],
+                [2, 4, 3],
+                [2, 1, 9],
+                [2, 1, 3],
+                [4, 4, 9],
+                [4, 4, 3],
+                [4, 1, 9],
+                [4, 1, 3],
+            ],
+        ),
+    ],
 )
 def test_combinations(args: List, expected: List[Any]):
     assert all_possible_lists.combinations(args) == expected
