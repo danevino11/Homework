79c5c6691bbaa97b75dffa36828fecd3b3dc8a0a
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-25 21:51:35 +0300

    Update test_cache_function.py

--- Homework_2/task4/tests/tests/test_cache_function.py
+++ Homework_2/task4/tests/tests/test_cache_function.py
@@ -6,6 +6,6 @@ from Homework_2.task4 import cache_function
 
 def test_function_cache():
     m = mock.Mock()
-    cache_func = cache(m)
+    cache_func = cache_function.cache(m)
     cache_func(100)
     assert m.call_count == 1
