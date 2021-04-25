1475f2e6dce1ce5860ae66a59779ec827df93a47
Danevich <73338391+danevino11@users.noreply.github.com>
2021-04-25 22:24:43 +0300

    Update test_range_function.py

--- Homework_2/task5/test/test_range_function.py
+++ Homework_2/task5/test/test_range_function.py
@@ -8,13 +8,11 @@ from Homework_2.task5 import range_function
 @pytest.mark.parametrize(
     ["args", "expected"],
     [
-        ([string.ascii_lowercase, "g"], ["a", "b", "c", "d", "e", "f"]),
         (
-            [string.ascii_lowercase, "g", "p"],
+            [string.ascii_lowercase, "g", "p", 1],
             ["g", "h", "i", "j", "k", "l", "m", "n", "o"],
         ),
-        ([string.ascii_lowercase, "p", "g", -2], ["p", "n", "l", "j", "h"]),
     ],
 )
-def test_custom_range(args: List, expected: List[Any]):
-    assert range_function.custom_range(args) == expected
+def test_custom_range(*args: List, expected: List[Any]):
+    assert range_function.custom_range(*args) == expected
